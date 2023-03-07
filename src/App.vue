22f4f746ef50e7a03bad7b302de359ef50514455
Rico Isaac Zamudio <zamudio.rico@gmail.com>
2023-03-08 06:01:56 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Home msg="Welcome to Your Vue.js App"/>
+
+  <Home/>
 </template>
 
 <script>
@@ -15,12 +15,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="hero">
+    <div class="hero__image-background">
+      <img src="https://via.placeholder.com/1920x650" alt="">
+    </div>
     <div class="hero__wrapper">
-
+      <div class="hero__wrapper-content">
+        <h1>Hello Developers!</h1>
+      </div>
     </div>
   </div>
 </template>
