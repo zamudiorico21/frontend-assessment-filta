810f2abd6293cc44683e95553192805e2e29dae3
unknown <>
2023-03-08 05:40:08 +0800

    Initial commit

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hero">
+    <div class="hero__wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroSection',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//Import SCSS
+import '@/scss/main.scss';
+
+createApp(App).mount('#app')
